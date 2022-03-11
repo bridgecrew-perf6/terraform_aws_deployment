12ab9c3ca0000e8068cadeca6c02cdf37d2725a1
Mathews Thomas <mathewsthomas001@gmail.com>
2022-03-11 08:37:49 +0000

    Added ec2 and s3

---
+++ s3-backend/variables.tf
@@ -0,0 +1,26 @@
+#Environment variables
+variable "AWS_ACCESS_KEY" {
+  default = "AKIAVJ2BTXXTKLMG3YCQ"
+}
+
+variable "AWS_SECRET_KEY" {
+  default = "oEC3xUjYjkcYmOObgGQBe+XeFXcct6DSFhvtDT8i"
+}
+
+variable "AWS_REGION" {
+  default = "eu-west-2"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    "eu-west-2" = "ami-0dd555eb7eb3b7c82"
+  }
+}
+
+variable "server_port" {
+  description = "Port used by the HTTP requests"
+  type        = number
+  default     = 8080
+}
+
